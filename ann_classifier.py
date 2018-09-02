1b20e3f01c60448151d2dec0931248d8062cbac9
caffeine110 <gaurav.developer.audio@gmail.com>
2018-09-02 18:37:13 +0530

    update

--- ann_classifier.py
+++ ann_classifier.py
@@ -6,19 +6,23 @@ aim : TO build Ann which predicts which employee is likely to leave the job
 date :18/6/18
 
 """
-#importing the libraries
 
+#PART -- 1 :
+
+#Phase - 1 : importing the libraries
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
 
+#Phase - 2: Prepering Datasets
 #importing datasets
 dataset = pd.read_csv('data_inp.csv')
 X = dataset.iloc[:,3:13].values
 Y = dataset.iloc[:,13].values
 
 
+#Phase - 3:
 #encoding the categerical data
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 labelencoder_X_1 = LabelEncoder()
@@ -38,6 +42,7 @@ from sklearn.model_selection import train_test_split
 X_train, X_test, Y_train, Y_test = train_test_split(X,Y,test_size = 0.25, random_state = 0)
 
 
+#Phase - 4 : 
 #feature_scalling
 from sklearn.preprocessing import StandardScaler
 sc = StandardScaler()
@@ -45,11 +50,14 @@ X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
 
 
-### PART 2
+
+#PART  -- 2 :
+#:
 #importing keras libraries and packages
 import keras
 from keras.models import Sequential
 from keras.layers import Dense
+
 
 #initialisation of ANN model
 classifier = Sequential()
@@ -69,8 +77,8 @@ classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics=['a
 classifier.fit(X_train, Y_train, batch_size = 10, nb_epoch = 100)
 
 
-#### PART 3
 
+# PART --- 3:
 
 #predicting the test set Results
 Y_pred = classifier.predict(X_test)
@@ -79,5 +87,3 @@ Y_pred = (Y_pred > 0.5)
 #making the confusion Metrix
 from sklearn.metrics import confusion_matrix
 cm = confusion_matrix(Y_test,Y_pred)
-
-
